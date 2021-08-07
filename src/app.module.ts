2a8ae94e6391ec51f2da765179c77a4625de6bd0
cheymos <nevesomov@gmail.com>
2021-08-08 00:23:07 +0600

    Create base Tag module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ormconfig } from './ormconfig';
+import { TagModule } from './tag/tag.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
+    TagModule,
   ],
   controllers: [AppController],
   providers: [AppService],
