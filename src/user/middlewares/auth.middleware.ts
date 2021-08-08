b4d97b12734939130e6ff64d88f46304a7e44985
cheymos <nevesomov@gmail.com>
2021-08-09 00:50:46 +0600

    Create and connect auth middleware

---
+++ src/user/middlewares/auth.middleware.ts
@@ -0,0 +1,33 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Response } from 'express';
+import { JwtPayload, verify } from 'jsonwebtoken';
+import { JWT_SECRET } from 'src/config';
+import { IExpressRequest } from 'src/types/expressRequest.interface';
+import { UserService } from '../user.service';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly userService: UserService) {}
+
+  async use(req: IExpressRequest, res: Response, next: NextFunction) {
+    if (!req.headers.authorization) {
+      req.user = null;
+      next();
+      return;
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    try {
+      const decode = verify(token, JWT_SECRET) as JwtPayload;
+
+      const user = await this.userService.findById(decode.id);
+
+      req.user = user;
+      next();
+    } catch (err) {
+      req.user = null;
+      next();
+    }
+  }
+}
