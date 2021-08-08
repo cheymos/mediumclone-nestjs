331649dc49ec490e820faf42ea6e996475ca2db1
cheymos <nevesomov@gmail.com>
2021-08-08 20:48:36 +0600

    Install class-validator and class-transformer and use validation pipe!

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { IUserResponse } from './types/user-response.interface';
 import { UserEntity } from './user.entity';
@@ -7,9 +8,12 @@ import { UserService } from './user.service';
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UsePipes(new ValidationPipe())
   @Post('users')
   async createUser(@Body('user') createUserDto: CreateUserDto): Promise<IUserResponse> {
     const user = await this.userService.createUser(createUserDto);
+
     return this.userService.buildUserResponse(user);
   }
 }
