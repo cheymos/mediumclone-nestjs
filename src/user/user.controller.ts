86226f76b7be33b707fc2b0f84bf3828beb426f9
cheymos <nevesomov@gmail.com>
2021-08-08 23:35:50 +0600

    Add POST endpoint for users/login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { ValidationPipe } from '@nestjs/common';
+import { HttpCode, ValidationPipe } from '@nestjs/common';
 import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { IUserResponse } from './types/user-response.interface';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
@@ -13,6 +14,15 @@ export class UserController {
   @Post('users')
   async createUser(@Body('user') createUserDto: CreateUserDto): Promise<IUserResponse> {
     const user = await this.userService.createUser(createUserDto);
+
+    return this.userService.buildUserResponse(user);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('users/login')
+  async login(@Body('user') loginUserDto: LoginUserDto): Promise<IUserResponse> {
+    const user = await this.userService.login(loginUserDto);
 
     return this.userService.buildUserResponse(user);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,8 @@ import { UserEntity } from './user.entity';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from 'src/config';
 import { IUserResponse } from './types/user-response.interface';
+import { LoginUserDto } from './dto/login-user.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -13,7 +15,7 @@ export class UserService {
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const userByEmail = await this.userRepository.findOne({ email: createUserDto.email });
-    const userByUsername = await this.userRepository.findOne({ username: createUserDto.username });
+    const userByUsername = await this.userRepository.findOne({ username: createUserDto.username }); // TODO: Check using querybuilder*
 
     if (userByEmail || userByUsername) {
       throw new HttpException('Email or username are taken', HttpStatus.UNPROCESSABLE_ENTITY);
@@ -23,6 +25,29 @@ export class UserService {
     Object.assign(newUser, createUserDto);
 
     return this.userRepository.save(newUser);
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne(
+      { email: loginUserDto.email },
+      { select: ['id', 'email', 'username', 'bio', 'image', 'password'] },
+    );
+
+    if (!user) {
+      throw new HttpException('Email or password is incorrect', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    console.log('user', user);
+
+    const isPasswordCorrect = await compare(loginUserDto.password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new HttpException('Email or password is incorrect', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    delete user.password;
+
+    return user;
   }
 
   buildUserResponse(user: UserEntity): IUserResponse {
