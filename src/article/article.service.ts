148ec0ebb99fcb1b8a85ac20172efaa0af0a3ea5
cheymos <nevesomov@gmail.com>
2021-08-11 21:17:00 +0600

    Add slug generation method

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import slugify from 'slugify';
 import { UserEntity } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
 import { ArticleEntity } from './article.entity';
@@ -15,7 +16,7 @@ export class ArticleService {
 
     Object.assign(article, createArticleDto);
 
-    article.slug = article.title.toLowerCase().split(' ').join('-');
+    article.slug = this.generateSlug(article.title);
     article.author = currentUser;
 
     return this.articleReposito.save(article);
@@ -23,5 +24,11 @@ export class ArticleService {
 
   buildArticleResponse(article: ArticleEntity): IArticleResponse {
     return { article };
+  }
+
+  private generateSlug(title: string): string {
+    const postfix = ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
+
+    return slugify(title, { lower: true }) + '-' + postfix;
   }
 }
