17560afcef156ae2de23ba4e57496cd8effcfea7
cheymos <nevesomov@gmail.com>
2021-08-11 20:35:50 +0600

    Create article entity

---
+++ src/article/article.entity.ts
@@ -0,0 +1,40 @@
+import { UserEntity } from 'src/user/user.entity';
+import { BeforeUpdate, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'articles' })
+export class ArticleEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text' })
+  slug: string;
+
+  @Column({ type: 'text' })
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'text' })
+  body: string;
+
+  @Column({ type: 'varchar', length: 100, array: true, nullable: true })
+  tagList: string[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column({ type: 'int', default: 0 })
+  favoritesCount: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.articles)
+  author: UserEntity;
+
+  @BeforeUpdate()
+  updateDate() {
+    this.updatedAt = new Date();
+  }
+}
