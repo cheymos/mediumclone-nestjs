94dea5384734df14c9bce381a2e2e5222345585b
cheymos <nevesomov@gmail.com>
2021-08-15 01:14:20 +0600

    Implement new feature - following on profile

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common';
 import { User } from 'src/user/decorators/user.decorator';
+import { AuthGuard } from 'src/user/guards/auth.guard';
+import { UserEntity } from 'src/user/user.entity';
 import { ProfileService } from './profile.service';
 import { IProfileResponse } from './types/profile-response.interface';
 
@@ -10,6 +12,14 @@ export class ProfileController {
   @Get(':username')
   async find(@Param('username') profileUsername: string, @User('id') currentUserId: number): Promise<IProfileResponse> {
     const profile = await this.profileService.find(profileUsername, currentUserId);
+
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(@User() currentUser: UserEntity, @Param('username') profileUsername: string): Promise<IProfileResponse> {
+    const profile = await this.profileService.followProfile(currentUser, profileUsername);
 
     return this.profileService.buildProfileResponse(profile);
   }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -2,24 +2,54 @@ import { HttpCode, HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
+import { FollowEntity } from './follow.entity';
 import { IProfileResponse } from './types/profile-response.interface';
 import { ProfileType } from './types/profile.type';
 
 @Injectable()
 export class ProfileService {
-  constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) {}
+  constructor(
+    @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity) private readonly followRepository: Repository<FollowEntity>,
+  ) {}
 
   async find(profileUsername: string, currentUserId: number): Promise<ProfileType> {
-    const user = await this.userRepository.findOne({ username: profileUsername });
-
-    if (!user) throw new HttpException('Profile did not found', HttpStatus.NOT_FOUND);
+    const user = await this.findProfileByUsername(profileUsername);
 
     return { ...user, following: false };
+  }
+
+  async followProfile(currentUser: UserEntity, profileUsername: string) {
+    const user = await this.findProfileByUsername(profileUsername);
+
+    if (user.id === currentUser.id) {
+      throw new HttpException('Follower and following cant be equal', HttpStatus.BAD_REQUEST);
+    }
+
+    const follow = await this.followRepository.findOne({ follower: currentUser, following: user });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.follower = currentUser;
+      followToCreate.following = user;
+
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
   }
 
   buildProfileResponse(profile: ProfileType): IProfileResponse {
     delete profile.email;
 
     return { profile };
+  }
+
+  private async findProfileByUsername(username: string): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user) throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+
+    return user;
   }
 }
